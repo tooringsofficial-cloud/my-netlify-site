5a1fd3924a4f297394f5648d068041beb121a6c3
Sanghyun Seong <antho@example.com>
2025-12-15 01:02:23 +0900

    Force Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     // Tailwind CSS 클래스를 사용하여 배경을 밝게, 텍스트를 검게 설정했습니다.
     <main className="bg-white p-8 min-h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-extrabold text-blue-700">PharmaD 첫 웹사이트!</h1>
+      <h1 className="text-4xl font-extrabold text-blue-700">PharmaD! 첫 웹사이트!</h1>
       <p className="mt-4 text-xl text-gray-600">
         Tailwind CSS와 Netlify로 성공적으로 배포 완료했습니다.
       </p>
